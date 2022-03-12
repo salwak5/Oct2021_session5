b9729c16aad4f49cbf47c8c73c9e981d82d19045
Salwa <salwaafreen5@gmail.com>
2022-03-12 14:47:54 -0600

    git trst 2

---
+++ src/main/java/variousconcepts5/gittest2.java
@@ -0,0 +1,10 @@
+package variousconcepts5;
+
+public class gittest2 {
+	public static void main(String[] args) {
+		System.out.println("abc");
+		System.out.println("xyz");
+		System.out.println("jfk");
+	}
+
+}
