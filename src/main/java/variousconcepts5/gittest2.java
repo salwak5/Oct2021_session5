2e81c63678030f081c387451067d68192a8ea17d
Salwa <salwaafreen5@gmail.com>
2022-03-12 14:54:45 -0600

    git test2

--- src/main/java/variousconcepts5/gittest2.java
+++ src/main/java/variousconcepts5/gittest2.java
@@ -5,6 +5,11 @@ public class gittest2 {
 		System.out.println("abc");
 		System.out.println("xyz");
 		System.out.println("jfk");
+		printme();
 	}
-
+public static void printme() {
+	System.out.println("print me");
+	System.out.println("print me also");
+	
+}
 }
